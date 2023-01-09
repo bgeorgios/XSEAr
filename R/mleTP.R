352da04f4cdda945e1513a5bb5a10520b923b1b2
Georgios Boumis <62819177+bgeorgios@users.noreply.github.com>
2023-01-08 21:27:48 -0600

    Create mleTP.R

---
+++ R/mleTP.R
@@ -0,0 +1,25 @@
+mleTP <- function(z, init.beta = NULL, init.theta = NULL) {
+  # z = numeric vector of data
+  # init.beta = initial value for beta parameter
+  # init.theta = initial value for theta parameter
+  
+  # get lower truncation threshold
+  u <- min(z)
+  
+  # return Tapered Pareto log-likelihood
+  logLikeTP <- function(par) {
+    n <- length(z)
+    ll <-
+      n * par[1] * log(u) + (1 / par[2]) * (n * u - sum(z)) - par[1] *
+      sum(log(z)) + sum(log(par[1] / z + 1 / par[2]))
+    return(-ll)
+  }
+  
+  # optimize beta & theta
+  mleproc <- optim(c(init.beta, init.theta), logLikeTP)
+  
+  # put parameters into list
+  parlist <- list("beta" = mleproc[["par"]][1], "theta" = mleproc[["par"]][2])
+  
+  return(parlist)
+}
